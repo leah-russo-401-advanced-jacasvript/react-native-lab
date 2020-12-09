a20b97cf4b231582b26d91a5fe5b5e098229fb6f
leahgrace555 <leah.grace555@gmail.com>
2020-12-09 00:25:09 -0500

    add view

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as Contacts from 'expo-contacts';
+
+
 
 export default function App() {
+
+  const [permissions, serPermissions] = useState(false);
+  const [Contacts, setContacts] = useState([]);
+
+  const getPermissions = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CONTACTS);
+    setPermissions(status === 'granted' ? true : false);
+  }
+
+  const getContacts = async () => {
+    const contactList = await Contacts.getContactsAsync();
+    setContacts(contactsList);
+  }
+
+  useEffect( ()=> {
+    getPermissions()
+  }, [])
   return (
+    <>
     <View style={styles.container}>
-      <Text>This is my first react native component!</Text>
-      <StatusBar style="auto" />
+      <Text>Click to view contacts</Text>
+      <Button 
+        onPress={getContacts}
+        title="Get Contcats"
+      />
     </View>
+
+    <View  style={styles.container}>
+      <FlatList data={contacts} renderItem={
+        (item)=> {
+          return <Button title={item.name}/>
+        }}>
+
+      </FlatList>
+    </View>
+
+    </>
   );
 }
 
